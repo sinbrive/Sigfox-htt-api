055b1c26b37ce9856635fef0949e661bdc19ed96
sinbrive <21102151+sinbrive@users.noreply.github.com>
2021-10-30 13:27:51 +0200

    Update config.java

--- sigfoxapi/config.java
+++ sigfoxapi/config.java
@@ -1,5 +1,5 @@
 class Secure  {
-static String user = "6165d7c5e83eeb9af52b";
-static String password = "0396093190d8e1bbfab79840f";
+static String user = "61655e83eeb9af52b";
+static String password = "0396093190d8fab79840f";
 
 }
