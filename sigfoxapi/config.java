c56c92b7a53fd6bdf565024fe41eac69ffd3bd74
sinbrive <sinbrive@gmail.com@users.noreply.github.com>
2021-10-30 15:23:44 +0200

    no message

--- sigfoxapi/config.java
+++ sigfoxapi/config.java
@@ -1,5 +1,5 @@
 class Secure  {
-static String user = "6165d7c";
-static String password = "039609";
+static String user = "6165d7c5";
+static String password = "039";
 static String device_id="3068E";
 }
